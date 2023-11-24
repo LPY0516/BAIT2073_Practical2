afddcbb23871a43569238be696868d5e2e144313
Loh Poh Yee <pohyeeloh12@gmail.com>
2023-11-24 14:33:20 +0800

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BAIT2073_Practical2"
+include(":app")
+ 
